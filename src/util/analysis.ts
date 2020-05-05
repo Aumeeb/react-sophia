7eb61a385e00495b437c6dd7b4d0723c492556c9
noberk <noberk@users.noreply.github.com>
2020-05-05 14:13:01 +0800

    Add a function which could get each parameter of user function which passed in

---
+++ src/util/analysis.ts
@@ -0,0 +1,14 @@
+function analyzeFuncParams(fn: Function): string[] {
+    if (typeof fn === "function") {
+        const funcString: string = fn.toString()
+        let parenthesisL = funcString.indexOf(String.fromCharCode(0x28))
+        let parenthesisR = funcString.indexOf(String.fromCharCode(0x29))
+        const paramsSeries = funcString.substr(parenthesisL + 1, parenthesisR - parenthesisL - 1)
+        return paramsSeries.split(',')
+    }
+    return []
+}
+export function recall() {
+
+}
+
